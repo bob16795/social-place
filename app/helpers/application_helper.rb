5db36cc6879176a0bd97482bb47d86bae5fdda19
bob16795 <prestosilver.ptp@gmail.com>
2020-04-05 21:16:02 +0000

    added redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {
+     filter_html:     false,
+     hard_wrap:       true,
+     link_attributes: { rel: 'nofollow', target: "_blank" },
+     space_after_headers: true,
+     fenced_code_blocks: true
+    }
+  
+    extensions = {
+     autolink:           true,
+     no_intra_emphasis: true,
+     superscript:        true,
+     highlight: true,
+     strikethrough: true,
+     quote: true,
+     no_images: true,
+     no_styles: true,
+     prettify: true,
+     superscript: true,
+     footnotes: true,
+     tables: true
+   }
+  
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
+  end
 end
